65086fc1b9e0f5779ea77ddfed3e568713c8481f
TimCauss <t.caussignac@gmail.com>
2023-12-27 12:23:48 +0100

    add: char.py container for  all characters functions, methods and variables

---
+++ Libs/chars.py
@@ -0,0 +1,12 @@
+
+# All var, functions and method concerning characters,
+# numerics and specials characters
+
+
+# Var
+alpha_min = "abcdefghijklmnopqrstuvwxyz"
+alpha_maj = "ABCDEJGHIJKLMNOPQRSTUVWXYZ"
+num = "0123456789"
+special_char = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~" + "\""
+
+all_chars = f"{alpha_min}{alpha_maj}{num}{special_char}"
