126ba723abcc28216a74ab45ff6494c7a0c169ba
TimCauss <t.caussignac@gmail.com>
2023-12-29 13:50:38 +0100

    Refactor: randomizer is now a module containing mains var and randomizer function

--- Libs/randomizer.py
+++
@@ -1,5 +0,0 @@
-
-# Function who return a string from args
-
-def randomizer(lenght: int, alpha=True, num=True, specials=True) -> str:
-    pass
--- Modules/randomizer.py
+++ Modules/randomizer.py
@@ -1,8 +1,4 @@
 
-# All var, functions and method concerning characters,
-# numerics and specials characters
-
-
 # Var
 alpha_min = "abcdefghijklmnopqrstuvwxyz"
 alpha_maj = "ABCDEJGHIJKLMNOPQRSTUVWXYZ"
@@ -10,3 +6,8 @@ num = "0123456789"
 special_char = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~" + "\""
 
 all_chars = f"{alpha_min}{alpha_maj}{num}{special_char}"
+
+
+# Function who return a string from args
+def randomizer(lenght: int, alpha=True, num=True, specials=True) -> str:
+    pass
