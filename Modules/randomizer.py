e94f84c4d49072d0276c814b640442b7376d743f
TimCauss <t.caussignac@gmail.com>
2024-01-02 12:32:11 +0100

    Debug: change append in concatenation, refactor return

--- Modules/randomizer.py
+++ Modules/randomizer.py
@@ -10,15 +10,13 @@ all_chars = f"{alpha_min}{alpha_maj}{num}{special_char}"
 
 
 # Function who return a string from args
-def randomizer(lenght: int, alpha_maj=True, num=True, specials=True) -> str:
-    word = ''
-    while len(word) < lenght:
-        if not (alpha_maj or num or specials):
-            word.append(random.choice(alpha_min))
-        if not (num or special_char):
-            word.append(random.choice(alpha_min+alpha_min))
-        if not (special_char):
-            word.append(random.choice(alpha_min+alpha_maj+num))
-        else:
-            word.append(random.choice(alpha_min+alpha_maj+num+special_char))
-    return word
+def randomizer(length: int, use_alpha_maj=True, use_num=True, use_specials=True) -> str:
+    characters = alpha_min
+    if use_alpha_maj:
+        characters += alpha_maj
+    if use_num:
+        characters += num
+    if use_specials:
+        characters += special_char
+
+    return ''.join(random.choice(characters) for _ in range(length))
