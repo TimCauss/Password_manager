47243c39ed2bcff623509315b3f6ee9e5698b39a
TimCauss <t.caussignac@gmail.com>
2024-01-02 14:16:50 +0100

    debug: "\"" -> '\"'

--- Modules/randomizer.py
+++ Modules/randomizer.py
@@ -4,7 +4,7 @@ import random
 alpha_min = "abcdefghijklmnopqrstuvwxyz"
 alpha_maj = "ABCDEJGHIJKLMNOPQRSTUVWXYZ"
 num = "0123456789"
-special_char = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~" + "\""
+special_char = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~" + '\"'
 
 all_chars = f"{alpha_min}{alpha_maj}{num}{special_char}"
 
