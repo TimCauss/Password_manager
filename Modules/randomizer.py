b38f9bb9462ca3acd627114b646efef5e441353b
TimCauss <t.caussignac@gmail.com>
2024-01-02 09:38:56 +0100

    feat: first iteration of randomizer function, return an randomize word from args

--- Modules/randomizer.py
+++ Modules/randomizer.py
@@ -1,3 +1,4 @@
+import random
 
 # Var
 alpha_min = "abcdefghijklmnopqrstuvwxyz"
@@ -9,5 +10,13 @@ all_chars = f"{alpha_min}{alpha_maj}{num}{special_char}"
 
 
 # Function who return a string from args
-def randomizer(lenght: int, alpha=True, num=True, specials=True) -> str:
-    pass
+def randomizer(lenght: int, alpha_maj=True, num=True, specials=True) -> str:
+    for i in range(lenght):
+        if not (alpha_maj or num or specials):
+            return random.choice(alpha_min)
+        if not (num or special_char):
+            return random.choice(alpha_min+alpha_min)
+        if not (special_char):
+            return random.choice(alpha_min+alpha_maj+num)
+        else:
+            return random.choice(alpha_min+alpha_maj+num+special_char)
