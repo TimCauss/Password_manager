6616a436412d86aceb70a109ac81401a9ed75a9d
TimCauss <t.caussignac@gmail.com>
2024-01-02 14:16:49 +0100

    update: regex search remplaced by re.fullmatch in test

--- Tests/test_randomizer.py
+++ Tests/test_randomizer.py
@@ -6,5 +6,5 @@ word_lenght = 5
 
 def test_randomizer_with_min_alpha_only():
     result = randomizer(word_lenght, False, False, False)
-    match = re.search(r'\b[a-z]{5}\b', result)
-    assert match is not None and len(match.group(0)) == word_lenght
+    match = re.fullmatch(r'\b[a-z]{5}\b', result)
+    assert match is not None
