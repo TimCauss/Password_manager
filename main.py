3080208acb8f5004b7d0b8009968ce517232c0e4
TimCauss <t.caussignac@gmail.com>
2024-01-04 20:55:36 +0100

    feat: adding checkbox for choosing randomizer args, starting grid interface

--- main.py
+++ main.py
@@ -6,16 +6,33 @@ class PasswordGenerator:
     def __init__(self):
         self.app = customtkinter.CTk()
         self.app.geometry("400x250")
+        self.grid_rowconfigure(0, weight=1)  # configure grid system
+        self.grid_columnconfigure(0, weight=1)
 
         self.slider_value: int = 10
-        self.alpha_maj: bool = True
-        self.nums: bool = True
-        self.special_chars = True
+        self.alpha_maj: bool = customtkinter.BooleanVar(value=True)
+        self.nums: bool = customtkinter.BooleanVar(value=True)
+        self.special_chars: bool = customtkinter.BooleanVar(value=True)
         self.word: str = ''
 
         self.button = customtkinter.CTkButton(
             self.app, text='Create', command=self.button_create)
         self.button.pack(padx=20, pady=20)
+
+        self.alpha_maj_checkbox = customtkinter.CTkCheckBox(
+            self.app, text="Maj.", variable=self.alpha_maj,
+        )
+        self.alpha_maj_checkbox.pack()
+
+        self.nums_checkbox = customtkinter.CTkCheckBox(
+            self.app, text="Numbers", variable=self.nums,
+        )
+        self.nums_checkbox.pack()
+
+        self.spec_chars_checkbox = customtkinter.CTkCheckBox(
+            self.app, text="Spec", variable=self.special_chars,
+        )
+        self.spec_chars_checkbox.pack()
 
         self.slider = customtkinter.CTkSlider(
             self.app, from_=10, to=50, number_of_steps=40,
@@ -42,8 +59,9 @@ class PasswordGenerator:
         self.label_len.configure(text=str(v))
 
     def button_create(self):
-        print(randomizer(self.slider_value))
-        self.word = randomizer(self.slider_value)
+        self.word = randomizer(self.slider_value, self.alpha_maj.get(),
+                               self.nums.get(), self.special_chars.get())
+
         self.text_box.configure(state="normal")
         self.text_box.delete("0.0", 'end')
         self.text_box.insert("0.0", str(self.word))
