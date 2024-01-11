1347483d0f0ed85c3fe5fcd29f2ca2caf12e067c
TimCauss <t.caussignac@gmail.com>
2024-01-11 17:52:39 +0100

    Creating password generator algo

--- Modules/randomizer.py
+++ Modules/randomizer.py
@@ -21,44 +21,27 @@ class PasswordCreator:
     Represents a password generator with configurable options.
     """
 
-    def __init__(self, length: int, use_alpha_maj=True,
-                 use_num=True, use_special_chars=True) -> object:
+    def __init__(self, length: int, use_upper_chars=40,
+                 use_num=20, use_special_chars=20) -> object:
         self.length = length
-        self.use_alpha_maj = use_alpha_maj
+        self.use_upper_chars = use_upper_chars
         self.use_num = use_num
         self.use_special_chars = use_special_chars
+        self.password_content = {}
+
+    def calcul_password_content(self) -> None:
+        """Calcul how many lowercase, uppercase, numbers and spe chars"""
+        if self.use_upper_chars:
+            self.password_content = {"uppers": (
+                self.length * self.use_upper_chars) / 100}
 
     def generate_password(self) -> str:
         """Generates a random password based on specified options."""
         characters = string.ascii_lowercase
-        if self.use_alpha_maj:
+        if self.use_upper_chars:
             characters += string.ascii_uppercase
         if self.use_num:
             characters += string.digits
         if self.use_special_chars:
             characters += string.punctuation
         return ''.join(secrets.choice(characters) for _ in range(self.length))
-
-    def add_uppercase(self) -> None:
-        """Enables the use of uppercase letters in generated passwords."""
-        self.use_alpha_maj = True
-
-    def remove_uppercase(self) -> None:
-        """Disables the use of uppercase letters in generated passwords."""
-        self.use_alpha_maj = False
-
-    def add_numbers(self) -> None:
-        """Enables the use of numbers in generated passwords."""
-        self.use_num = True
-
-    def remove_numbers(self) -> None:
-        """Disables the use of numbers in generated passwords."""
-        self.use_num = False
-
-    def add_special_chars(self) -> None:
-        """Enables the use of special characters in generated passwords."""
-        self.use_special_chars = True
-
-    def remove_special_chars(self) -> None:
-        """Disables the use of special characters in generated passwords."""
-        self.use_special_chars = False
