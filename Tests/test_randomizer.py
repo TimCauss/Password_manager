9637adb68f420092c4d81ddaa0deae116ad020a6
TimCauss <t.caussignac@gmail.com>
2024-01-12 19:39:54 +0100

    adding some tests

--- Tests/test_randomizer.py
+++ Tests/test_randomizer.py
@@ -1,5 +1,7 @@
-from Modules.randomizer import randomizer
+from Modules.PasswordCreator import randomizer, PasswordCreator
 import re
+import sys
+
 
 word_lenght = 5
 
@@ -8,3 +10,11 @@ def test_randomizer_with_min_alpha_only():
     result = randomizer(word_lenght, False, False, False)
     match = re.fullmatch(r'\b[a-z]{5}\b', result)
     assert match is not None
+
+
+for _ in range(10):
+    print(f"Password {_}: {randomizer(25)}")
+
+for _ in range(10):
+    password = PasswordCreator(25)
+    print(f"Password {_}: {password.password}")
